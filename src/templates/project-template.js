f1a0d523b608b81926ad0f4156dcbc3dc539abad
Anna <anna.b.scislowska@gmail.com>
2022-10-16 22:03:29 +0200

    children, links

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -7,7 +7,7 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/Layout"
 import VisitWebsiteIcon from "../images/icons/portfolio4.svg"
 
-const projecttemplate = ({ data }) => {
+const projecttemplate = ({ data, children }) => {
   //single post info:
   const {
     frontmatter: { title, tags, link, image },
@@ -15,7 +15,43 @@ const projecttemplate = ({ data }) => {
   //for Projectslist component:
   const { nodes: projectslist } = data.allMdx
 
-  return <div>{title}</div>
+  return (
+    <Layout>
+      <div className="project-page">
+        <div className="img-wrapper">
+          <a href={link} className="site-link" target="_blank">
+            <GatsbyImage
+              image={getImage(image)}
+              className="main-img"
+              alt={title}
+            />
+          </a>
+        </div>
+        <h1>{title}</h1>
+        <div className="project-info">
+          <div className="description">
+            <MDXProvider>{children}</MDXProvider>
+          </div>
+          <div className="details">
+            <a href={link} className="site-link" target="_blank">
+              <VisitWebsiteIcon className="icon" />
+              <span>Zobacz stronę</span>
+            </a>
+            <div>
+              <h3>Technologie: </h3>
+              <div className="tags">
+                {tags.map(tag => {
+                  return <span className="tag">{tag}</span>
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+        <h2>Inne projekty</h2>
+        {/* <Projectslist projects={projectslist} /> */}
+      </div>
+    </Layout>
+  )
 }
 export const query = graphql`
   query ($id: String!) {
